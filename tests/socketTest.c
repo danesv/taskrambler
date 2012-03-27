f952a214aa64606392cfdcf976e703547e39fdfc
Georg Hopp <georg@steffers.org>
2012-03-27 13:21:06 +0200

    some tests for socket class

--- tests/socketTest.c
+++ tests/socketTest.c
@@ -3,40 +3,30 @@
 #include <unistd.h>
 
 #include "runtest.h"
-#include "logger.h"
-#include "cclass.h"
+#include "class.h"
 #include "socket.h"
+#include "logger.h"
+#include "mock/mock_logger.h"
 
 
 #define TEST_PORT	11212
 
 
-int    level  = -1;
-char * msg    = NULL;
-
-static void
-logfnct_mock(int _level, const char * _msg)
-{
-    level = _level;
-    msg   = malloc(strlen(_msg) + 1);
-    strcpy(msg, _msg);
-}
-
 const char testname[] = "socketTest";
-LOGGER logger = NULL;
-SOCK   sock   = NULL;
+
+MockLogger logger = NULL;
+Sock       sock   = NULL;
 
 static
 int
 __setUp()
 {
-	logger = new(LOGGER, NULL);
-	logger_add(logger, logfnct_mock);
+	logger = new(MockLogger, LOGGER_DEBUG);
 
-	sock = new(SOCK, logger, TEST_PORT);
+	sock = new(Sock, logger, TEST_PORT);
 
-	ASSERT_INSTANCE_OF(SOCK, sock);
-	ASSERT_INSTANCE_OF(LOGGER, sock->logger);
+	ASSERT_INSTANCE_OF(Sock, sock);
+	ASSERT_INSTANCE_OF(MockLogger, sock->log);
 	ASSERT_EQUAL(TEST_PORT, sock->port);
 	ASSERT_NOT_EQUAL(0, sock->handle);
 
@@ -48,21 +38,14 @@ static
 int
 __tearDown()
 {
-    level = -1;
-
-    if (NULL != msg) {
-        free(msg);
-        msg = NULL;
-    }
-
     if (NULL != logger) {
         ASSERT_OBJECT(logger);
-        delete(&logger);
+        delete(logger);
     }
 
     if (NULL != sock) {
         ASSERT_OBJECT(sock);
-        delete(&sock);
+        delete(sock);
     }
 
     return TEST_OK;
@@ -73,29 +56,29 @@ static
 int
 testListen()
 {
-	sock_listen(sock, 10);
+	socketListen(sock, 10);
 
 	return TEST_OK;
 }
 
 static
 int
-testAccept()
+testAcceptConnect()
 {
-	SOCK acc, con;
+	Sock acc, con;
 	char addr[16];
 	pid_t pid;
 
-	sock_listen(sock, 10);
+	socketListen(sock, 10);
 
 	pid = fork();
 
 	switch(pid) {
 		case 0:
-			con = new(SOCK, logger, TEST_PORT);
+			con = new(Sock, logger, TEST_PORT);
 			sleep(1);
-			sock_connect(con, "127.0.0.1");
-			delete(&con);
+			socketConnect(con, "127.0.0.1", &addr);
+			delete(con);
 			__tearDown();
 			exit(EXIT_SUCCESS);
 
@@ -103,12 +86,12 @@ testAccept()
 			return TEST_FAILED;
 
 		default:
-			acc = sock_accept(sock, addr);
+			acc = socketAccept(sock, &addr);
 	}
 
     if (NULL != acc) {
         ASSERT_OBJECT(acc);
-        delete(&acc);
+        delete(acc);
     }
 
 	return TEST_OK;
@@ -116,7 +99,7 @@ testAccept()
 
 const testfunc tests[] = {
     testListen,
-	testAccept
+	testAcceptConnect
 };
 const size_t count = FUNCS_COUNT(tests);
 
