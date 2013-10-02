f6ce303d7bd2943d46dafddab4a538fde49042c0
Georg Hopp <georg@steffers.org>
2013-10-02 20:31:31 +0100

    load partials via javascript, so I do not need the server at all. closes #35

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,57 +1,68 @@
 var sess = null;
 
 $(document).ready(function() {
-	var sval    = new ServerVal("#randval");
+	var sval = new ServerVal("#randval");
 
-	sess = new Session("#sessinfo", "#sessid", "#user");
+	$("#title").load("/_title.html");
+	$("#footer").load("/_footer.html");
 
-	$.getJSON(
-		"/version/",
-		function(data) {
-			$("#version").empty().append("version: " + data.version);
-		}
-	);
+	$("#menu").load("/_menu.html", function() {
+		$("div#menu ul li.signup").click(function() {
+			$("#signup").removeClass("hide");
+		});
 
-	$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
-	$.getJSON("/user/get/", $.proxy(sess.loadUserJSON, sess));
+		$("div#menu ul li.login").click(function() {
+			$("#login").removeClass("hide");
+		});
+	});
 
-	$(window).focus(function() {
+	$("#statusline").load("/_statusline.html", function (){
+		sess = new Session("#sessinfo", "#sessid", "#user");
+
+		$.getJSON(
+			"/version/",
+			function(data) {
+				$("#version").empty().append("version: " + data.version);
+			}
+		);
+
 		$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
-	});
+		$.getJSON("/user/get/", $.proxy(sess.loadUserJSON, sess));
 
-	$("div#menu ul li.signup").click(function() {
-		$("#signup").removeClass("hide");
-	});
+		$(window).focus(function() {
+			$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
+		});
 
-	$("div#menu ul li.login").click(function() {
-		$("#login").removeClass("hide");
-	});
+		$("div#menu ul li.logout").click(function() {
+			$.getJSON("/logout/", $.proxy(sess.loadUserJSON, sess));
+			$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
+		});
 
-	$("div#menu ul li.logout").click(function() {
-		$.getJSON("/logout/", $.proxy(sess.loadUserJSON, sess));
-		$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
+		$("#login").load("/_login.html", function (){
+			$("#login form").submit(function(event) {
+				event.preventDefault();
+				$.post("/login/",
+					$("#login form").serialize(),
+					$.proxy(sess.loadUserJSON, sess));
+				$("#login").addClass("hide");
+				$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
+			});
+		});
+
+		$("#signup").load("/_signup.html", function (){
+			$("#signup form").submit(function(event) {
+				event.preventDefault();
+				$.post("/signup/",
+					$("#signup form").serialize(),
+					$.proxy(sess.loadUserJSON, sess));
+				$("#signup").addClass("hide");
+				$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
+			});
+		});
 	});
 
 	$("#randval").click(function() {
 		sval.stop();
-	});
-
-	$("#login form").submit(function(event) {
-		event.preventDefault();
-		$.post("/login/",
-			$("#login form").serialize(),
-			$.proxy(sess.loadUserJSON, sess));
-		$("#login").addClass("hide");
-		$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
-	});
-
-	$("#signup form").submit(function(event) {
-		event.preventDefault();
-		$.post("/signup/",
-			$("#signup form").serialize(),
-			$.proxy(sess.loadUserJSON, sess));
-		$("#signup").addClass("hide");
-		$.getJSON("/sessinfo/", $.proxy(sess.loadJSON, sess));
 	});
 });
 
