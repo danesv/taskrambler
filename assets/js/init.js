4825831ca748d132197e954b8a0b7d7c41397d97
Georg Hopp <georg@steffers.org>
2013-10-05 21:38:53 +0100

    now use one layout file and load even the content as a partial

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,29 @@
 var sess = null;
 
 $(document).ready(function() {
-	var sval = new ServerVal("#randval");
+	var sval      = new ServerVal("#randval");
+	var asset_exp = /^.*\/(.*)/;
+	var asset     = '/_main.html';
+
+	var title_adds = {
+		'/_author.html'        : ' - Author',
+		'/_documentation.html' : ' - Documentation',
+		'/_download.html'      : ' - Download',
+	};
+
+	asset_exp.exec(this.location);
+
+	if ('' != RegExp.$1) {
+		asset = '/_' + RegExp.$1;
+	}
+
+	if (asset in title_adds) {
+		this.title += title_adds[asset];
+	}
 
 	$("#title").load("/_title.html");
 	$("#footer").load("/_footer.html");
+	$("#main").load(asset);
 
 	$("#menu").load("/_menu.html", function() {
 		$("div#menu ul li.signup").click(function() {
